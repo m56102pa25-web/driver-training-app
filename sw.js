d0729a6adbf2fd935892701ca96bcac563ad7b0c
柏原　守 <kashiharamamoru@m56102-MacBook.local>
2026-06-29 09:24:18 +0900

    revert: 同期スクロールを廃止し直前の安定版に戻す

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jfl-guide-v9';
+const CACHE_NAME = 'jfl-guide-v10';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
