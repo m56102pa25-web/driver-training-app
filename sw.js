3e458e1d07f5ba9a0b4778a71c60a94844b1ed9d
柏原　守 <kashiharamamoru@m56102-MacBook.local>
2026-06-29 09:01:13 +0900

    feat: タブ切替にスライドアニメーション追加

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jfl-guide-v4';
+const CACHE_NAME = 'jfl-guide-v5';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
