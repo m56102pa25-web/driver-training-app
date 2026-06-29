3fceda63425b3907a5e7a99963d1fed7bfbf0b1b
柏原　守 <kashiharamamoru@m56102-MacBook.local>
2026-06-29 09:46:11 +0900

    feat: scroll-snap横スワイプ実装・タブとコンテンツを同期

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jfl-guide-v14';
+const CACHE_NAME = 'jfl-guide-v15';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
